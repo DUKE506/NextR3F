8a01c6fac63020ec98bcbe93543d36daef2abd83
duke <dukebs1128@gmail.com>
2024-12-19 17:03:41 +0900

    feat : 배포 오류 수정중

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-
+    output: 'export',  // 정적 사이트로 내보내기
+    basePath: '/NextR3F',  // GitHub Pages의 경로를 설정 (예: /NextR3F)
+    assetPrefix: '/NextR3F/public',  
 };
 
 export default nextConfig;
