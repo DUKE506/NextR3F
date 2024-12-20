1198cac2fcd3e874855fb52049d628aa4bcf9e0f
duke <dukebs1128@gmail.com>
2024-12-20 09:54:24 +0900

    feat : 수정4

--- next.config.mjs
+++ next.config.mjs
@@ -2,29 +2,33 @@
 const nextConfig = {
     reactStrictMode: true,
     trailingSlash: true,
-    async rewrites() {
+    async headers() {
         return [
             {
                 source: '/models/:path*',
-                destination: '/public/models/:path*',  // public 폴더의 models 디렉토리로 연결
-            }
+                headers: [
+                    {
+                        key: 'Content-Type',
+                        value: 'model/gltf-binary'
+                    }
+                ],
+            },
         ]
     },
     webpack: (config) => {
-    config.externals.push({
-        'sharp': 'commonjs sharp',
-        'canvas': 'commonjs canvas'
-    })
+        config.externals.push({
+            'sharp': 'commonjs sharp',
+            'canvas': 'commonjs canvas'
+        })
 
-    config.module.rules.push({
-        test: /\.(glb|gltf)$/,
-        type: 'asset/resource',
-        generator: {
-            // public 폴더 내의 models 디렉토리에 있는 파일들을 처리
-            filename: 'static/models/[name][ext]'  // [hash] 제거하고 원본 파일명 유지
-        }
-    })
-    return config
+        config.module.rules.push({
+            test: /\.(glb|gltf)$/,
+            type: 'asset/resource',
+            generator: {
+                filename: 'models/[name][ext]'
+            }
+        })
+        return config
     }
 };
 
